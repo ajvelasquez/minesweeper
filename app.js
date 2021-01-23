11d9a5a8838bd4496b6a7211370f1a0b6004aba8
avelasquez <amalio1212@gmail.com>
2021-01-23 18:59:20 -0400

    add basic logic to play the game and check for the win

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     let width = 10;
     let bombAmount = 20;
+    let flags = 0;
     let squares = [];
+    let isGameOver = false;
 
     function createBoard() {
         const bombsArray = Array(bombAmount).fill('bomb');
@@ -16,8 +18,188 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
+
+            square.addEventListener('click', () => {
+                click(square);
+            });
+
+            square.oncontextmenu = (e) => {
+                e.preventDefault();
+                addFlag(square);
+            };
+        }
+
+        //add numbers
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0;
+            const isLeftEdge = i % width === 0;
+            const isRightEdge = i % width === width - 1;
+
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i > 10 && squares[i - width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                if (i < 89 && squares[i + width].classList.contains('bomb')) {
+                    total++;
+                }
+
+                squares[i].setAttribute('data', total);
+            }
         }
     }
 
     createBoard();
+
+    function addFlag(square) {
+        if (isGameOver) {
+            return;
+        }
+
+        if (!square.classList.contains('checked') && flags < bombAmount) {
+            if (!square.classList.contains('flag')) {
+                square.classList.add('flag');
+                square.innerHTML = '🏴';
+                flags++;
+                checkForWin();
+            } else {
+                square.classList.remove('flag');
+                square.innerHTML = '';
+                flags--;
+            }
+        }
+    }
+
+    function click(square) {
+        let currentId = square.id;
+        
+        if (isGameOver) {
+            return;
+        }
+
+        if (square.classList.contains('checked') || square.classList.contains('flag')) {
+            return;
+        }
+
+        if (square.classList.contains('bomb')) {
+            gameOver(square);
+            return;
+        } else {
+            let total = square.getAttribute('data');
+            if (total != 0) {
+                square.classList.add('checked');
+                square.innerHTML = total;
+                return;
+            }
+    
+            checkSquare(square, currentId);
+        }
+
+        square.classList.add('checked');
+    }
+
+    //check neighboring squares
+    function checkSquare(square, currentId) {
+        const isLeftEdge = currentId % width === 0;
+        const isRightEdge = currentId % width === width - 1;
+
+        setTimeout(() => {
+            if (currentId > 0 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) - 1].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId > 9 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) + 1 - width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId > 10) {
+                const newId = squares[parseInt(currentId) - width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId > 11 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) - 1 - width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId < 98 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) + 1].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId < 90 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) - 1 + width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId < 88 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) + 1 + width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+
+            if (currentId < 89) {
+                const newId = squares[parseInt(currentId) + width].id;
+                const newSquare = document.getElementById(newId);
+                click(newSquare);
+            }
+        }, 10);
+    }
+
+    function gameOver (square) {
+        isGameOver = true;
+
+        squares.forEach((square) => {
+            if (square.classList.contains('bomb')) {
+                square.innerHTML = '💣';
+            }
+        });
+    }
+
+    function checkForWin() {
+        let matches = 0;
+
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
+                matches++;
+            }
+
+            if (matches === bombAmount) {
+                alert('You win');
+                isGameOver = true;
+            }
+        }
+    }
 });
